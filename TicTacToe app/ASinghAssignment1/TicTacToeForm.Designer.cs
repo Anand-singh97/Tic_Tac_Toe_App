9976414182dcf17b05a742d78287731700894c5c
Anand Singh <132922327+Anand-singh97@users.noreply.github.com>
2023-09-27 00:48:36 -0400

    Add files via upload

---
+++ TicTacToe app/ASinghAssignment1/TicTacToeForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ASinghAssignment1
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.pbx3 = new System.Windows.Forms.PictureBox();
+            this.pbx4 = new System.Windows.Forms.PictureBox();
+            this.pbx5 = new System.Windows.Forms.PictureBox();
+            this.pbx6 = new System.Windows.Forms.PictureBox();
+            this.pbx7 = new System.Windows.Forms.PictureBox();
+            this.pbx8 = new System.Windows.Forms.PictureBox();
+            this.pbx9 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbxTicTac = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).BeginInit();
+            this.gbxTicTac.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbx1
+            // 
+            this.pbx1.BackColor = System.Drawing.Color.Lavender;
+            this.pbx1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx1.Location = new System.Drawing.Point(291, 34);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(200, 200);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 0;
+            this.pbx1.TabStop = false;
+            this.pbx1.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx2
+            // 
+            this.pbx2.BackColor = System.Drawing.Color.Lavender;
+            this.pbx2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx2.Location = new System.Drawing.Point(515, 34);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(200, 200);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 0;
+            this.pbx2.TabStop = false;
+            this.pbx2.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx3
+            // 
+            this.pbx3.BackColor = System.Drawing.Color.Lavender;
+            this.pbx3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx3.Location = new System.Drawing.Point(739, 34);
+            this.pbx3.Name = "pbx3";
+            this.pbx3.Size = new System.Drawing.Size(200, 200);
+            this.pbx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx3.TabIndex = 0;
+            this.pbx3.TabStop = false;
+            this.pbx3.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx4
+            // 
+            this.pbx4.BackColor = System.Drawing.Color.Lavender;
+            this.pbx4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx4.Location = new System.Drawing.Point(291, 254);
+            this.pbx4.Name = "pbx4";
+            this.pbx4.Size = new System.Drawing.Size(200, 200);
+            this.pbx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx4.TabIndex = 0;
+            this.pbx4.TabStop = false;
+            this.pbx4.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx5
+            // 
+            this.pbx5.BackColor = System.Drawing.Color.Lavender;
+            this.pbx5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx5.Location = new System.Drawing.Point(515, 254);
+            this.pbx5.Name = "pbx5";
+            this.pbx5.Size = new System.Drawing.Size(200, 200);
+            this.pbx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx5.TabIndex = 0;
+            this.pbx5.TabStop = false;
+            this.pbx5.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx6
+            // 
+            this.pbx6.BackColor = System.Drawing.Color.Lavender;
+            this.pbx6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx6.Location = new System.Drawing.Point(739, 254);
+            this.pbx6.Name = "pbx6";
+            this.pbx6.Size = new System.Drawing.Size(200, 200);
+            this.pbx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx6.TabIndex = 0;
+            this.pbx6.TabStop = false;
+            this.pbx6.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx7
+            // 
+            this.pbx7.BackColor = System.Drawing.Color.Lavender;
+            this.pbx7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx7.Location = new System.Drawing.Point(291, 474);
+            this.pbx7.Name = "pbx7";
+            this.pbx7.Size = new System.Drawing.Size(200, 200);
+            this.pbx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx7.TabIndex = 0;
+            this.pbx7.TabStop = false;
+            this.pbx7.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx8
+            // 
+            this.pbx8.BackColor = System.Drawing.Color.Lavender;
+            this.pbx8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx8.Location = new System.Drawing.Point(515, 474);
+            this.pbx8.Name = "pbx8";
+            this.pbx8.Size = new System.Drawing.Size(200, 200);
+            this.pbx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx8.TabIndex = 0;
+            this.pbx8.TabStop = false;
+            this.pbx8.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // pbx9
+            // 
+            this.pbx9.BackColor = System.Drawing.Color.Lavender;
+            this.pbx9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx9.Location = new System.Drawing.Point(739, 474);
+            this.pbx9.Name = "pbx9";
+            this.pbx9.Size = new System.Drawing.Size(200, 200);
+            this.pbx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx9.TabIndex = 0;
+            this.pbx9.TabStop = false;
+            this.pbx9.Click += new System.EventHandler(this.pictureBox_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(525, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(411, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tic Tac Toe🕹️";
+            // 
+            // gbxTicTac
+            // 
+            this.gbxTicTac.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.gbxTicTac.Controls.Add(this.pbx9);
+            this.gbxTicTac.Controls.Add(this.pbx1);
+            this.gbxTicTac.Controls.Add(this.pbx2);
+            this.gbxTicTac.Controls.Add(this.pbx6);
+            this.gbxTicTac.Controls.Add(this.pbx4);
+            this.gbxTicTac.Controls.Add(this.pbx3);
+            this.gbxTicTac.Controls.Add(this.pbx5);
+            this.gbxTicTac.Controls.Add(this.pbx8);
+            this.gbxTicTac.Controls.Add(this.pbx7);
+            this.gbxTicTac.Location = new System.Drawing.Point(108, 107);
+            this.gbxTicTac.Name = "gbxTicTac";
+            this.gbxTicTac.Size = new System.Drawing.Size(1248, 746);
+            this.gbxTicTac.TabIndex = 2;
+            this.gbxTicTac.TabStop = false;
+            this.gbxTicTac.Text = "TicTacToe";
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(1400, 878);
+            this.Controls.Add(this.gbxTicTac);
+            this.Controls.Add(this.label1);
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe Application";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).EndInit();
+            this.gbxTicTac.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.PictureBox pbx3;
+        private System.Windows.Forms.PictureBox pbx4;
+        private System.Windows.Forms.PictureBox pbx5;
+        private System.Windows.Forms.PictureBox pbx6;
+        private System.Windows.Forms.PictureBox pbx7;
+        private System.Windows.Forms.PictureBox pbx8;
+        private System.Windows.Forms.PictureBox pbx9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbxTicTac;
+    }
+}
+
---
+++ TicTacToe app/ASinghAssignment1/TicTacToeForm.cs
@@ -0,0 +1,190 @@
+﻿/* PROGRAMMER - AnandPravesh Singh
+ * ID - 8884337
+ * PROFESSOR - Sabbir Ahmed
+ * DESCRIPTION - Tic_Tac_Toe Game
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASinghAssignment1
+{
+    public partial class TicTacToeForm : Form
+    {
+        private string[,] matrix = new string[3, 3];
+        private Dictionary<string, (int, int)> pairs = new Dictionary<string, (int, int)>();
+        private int counter = 0;
+
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+            FillDictionary();
+        }
+
+        private void pictureBox_click(object sender, EventArgs e)
+        {
+            if (((PictureBox)sender).Image == null)
+            {
+                if (counter % 2 != 0)
+                {
+                    ((PictureBox)sender).Image = Resource1.X_logo;
+                    
+                    updateMatrix("X", ((PictureBox)sender).Name);
+                }
+                else
+                {
+                    ((PictureBox)sender).Image = Resource1.O_logo;
+
+                    updateMatrix("O", ((PictureBox)sender).Name);
+                }
+
+                counter++;
+                if (counter >= 5)
+                {
+                    checkWinner();
+                }
+            }
+        }
+
+        public void FillDictionary()
+        {
+            int x = 1;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    pairs.Add("pbx" + x, (i, j));
+                    x++;
+                }
+            }
+        }
+
+        private void updateMatrix(string userLogo, string boxName)
+        {
+            matrix[pairs[boxName].Item1, pairs[boxName].Item2] = userLogo;
+        }
+
+        private void checkWinner()
+        {
+            string result = "";
+
+            //Check Horozontally.
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == null)
+                    {
+                        break;
+                    }
+                    result += matrix[i, j];
+                }
+                if (result == "XXX" || result == "OOO")
+                {
+                    MessageBox.Show(result[0].ToString() + " Wins!!");
+                    resetGame();
+                    break;
+                }
+                result = "";
+            }
+
+            //Check Vertically.
+            if (result != "XXX" && result != "OOO")
+            {              
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        if (matrix[i, j] == null)
+                        {
+                            break;
+                        }
+
+                        result += matrix[i, j];
+                    }
+                    if (result == "XXX" || result == "OOO")
+                    {
+                        MessageBox.Show(result[0].ToString() + " Wins!!");
+                        resetGame();
+                        break;
+                    }
+                    result = "";
+                }
+            }
+
+            //Check Diagonally (Left to Right)
+            if (result != "XXX" && result != "OOO")
+            {        
+                for (int j = 0, i = 0; j < matrix.GetLength(0); j++, i++)
+                {
+                    if (matrix[i, j] == null)
+                    {
+                        break;
+                    }
+                    result += matrix[i, j];
+                }
+
+                if (result == "XXX" || result == "OOO")
+                {
+                    MessageBox.Show(result[0].ToString() + " Wins!!");
+                    resetGame();
+                }
+                result = "";
+            }
+
+            //Check Diagonally (Right to Left)
+            if (result != "XXX" && result != "OOO")
+            {
+                for (int j = 2, i = 0; i < matrix.GetLength(0); j--, i++)
+                {
+                    if (matrix[i, j] == null)
+                    {
+                        break;
+                    }
+                    result += matrix[i, j];
+                }
+
+                if (result == "XXX" || result == "OOO")
+                {
+                    MessageBox.Show(result[0].ToString() + " Wins!!");
+                    resetGame();
+                }
+                result = "";
+            }
+
+            //Check if Draw!!
+            if (counter == 9 && result != "XXX" && result != "OOO")
+            {
+                MessageBox.Show("Draw!!");
+                resetGame();
+            }
+        }
+
+        private void resetGame()
+        {
+            foreach (PictureBox pb in gbxTicTac.Controls)
+            {
+
+                pb.Image = null;
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = null;
+                }
+            }
+
+            counter = 0;
+        }
+    }
+}
